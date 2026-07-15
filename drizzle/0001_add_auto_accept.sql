fd93ad4a341cbc2d8392d5262bf2a01f942c1d1e
Enri-code <onyeuloeric@gmail.com>
2026-07-15 20:09:58 +0100

    feat: add autoAcceptDispatchers/autoAcceptRiders fields to company_settings

---
+++ drizzle/0001_add_auto_accept.sql
@@ -0,0 +1,2 @@
+ALTER TABLE company_settings ADD COLUMN auto_accept_dispatchers boolean DEFAULT false NOT NULL;
+ALTER TABLE company_settings ADD COLUMN auto_accept_riders boolean DEFAULT false NOT NULL;
